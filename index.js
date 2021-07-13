910e3e34e4d3acb8d6733678f0f1b66fbbd2c52b
hibasalem <salemhiba.hs@gmail.com>
2021-07-13 22:10:07 +0300

    edits

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const { v4: uuidv4 } = require('uuid');
 // const { Socket } = require('dgram');
 
 io.listen(server);
-const queue ={
-  allGames:[],
-  allPlayers:[],
-}
+const queue = {
+  allGames: [],
+  allPlayers: [],
+};
 
 app.use(cors());
 app.get('/test', (req, res) => {
@@ -28,48 +28,45 @@ io.on('connection', (socket) => {
   console.log('hello connect');
 
   socket.on('join', (payload) => {
-    const player = {name: payload.name, id: socket.id};
+    const player = { name: payload.name, id: socket.id };
     queue.allPlayers.push(player);
     socket.join(gameRoom);
-    socket
-      .to(gameRoom)
-      .emit('onlineGamers',player);
+    socket.to(gameRoom).emit('onlineGamers', player);
   });
 
   socket.on('createGame', (payload) => {
-    const gameData = {...payload, id: uuidv4(), socketId: socket.id};
+    const gameData = { ...payload, id: uuidv4(), socketId: socket.id };
     queue.allGames.push(gameData);
     console.log('hello from create backend', payload);
-    socket
-    .in(gameRoom)
-    .emit('newGame', gameData);
-   
-    
+    socket.in(gameRoom).emit('newGame', gameData);
+
     console.log('hello from create ', {
       ...payload,
-      id: uuidv4 (),
+      id: uuidv4(),
       socketId: socket.id,
-    } )
-    console.log('Queue',queue);
+    });
+    console.log('Queue', queue);
   });
 
   socket.on('claim', (payload) => {
     console.log('hello claim backend');
-    socket.to(payload.playerId).emit('claimed',{name: payload.name });
-    queue.allGames=quque.allGames.filter((item) => item.id !== payload.id);
+    socket.to(payload.playerId).emit('claimed', { name: payload.name });
+    queue.allGames = queue.allGames.filter((item) => item.id !== payload.id);
   });
   socket.on('getAll', () => {
     queue.allPlayers.forEach((player) => {
-      socket.emit('onlineGamers', {name : player.name, id : player.id});
+      socket.emit('onlineGamers', { name: player.name, id: player.id });
     });
     queue.allGames.forEach((game) => {
-      socket.emit('newGame',game);
+      socket.emit('newGame', game);
     });
   });
 
   socket.on('disconnect', () => {
     socket.to(gameRoom).emit('offlineGamers', { id: socket.id });
-    queue.allPlayers = queue.allPlayers.filter((player) => player.id !== socket.id);
+    queue.allPlayers = queue.allPlayers.filter(
+      (player) => player.id !== socket.id
+    );
   });
 });
 
